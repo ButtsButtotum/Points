f009baba49cfa39c5bf234eb88a2ad7dc528abbe
butts <butts.and.butts@gmail.com>
2014-02-10 15:24:09 -0500

    .

--- Points/src/pm/points/tests/BasicTest.java
+++ Points/src/pm/points/tests/BasicTest.java
@@ -1,7 +1,16 @@
 package pm.points.tests;
 
+import java.util.Set;
+
+import pm.points.basic.Point;
+import pm.points.forces.AbstractPointForce;
+
 public class BasicTest
 {
+	
+	private Set<Point> thePoints;
+	private Set<AbstractPointForce> theForces;
+	
 	
 	public static void main(String[] args) throws Throwable {
 		System.out.println("Testing has begun.");
